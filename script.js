0555555c4e27d4deaf71b9a074e230b461afb1fd
MohammadHHaj <muddihhaj@hotmail.com>
2024-10-23 22:35:54 +0200

    h

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let users = {Firstname: 'Mohammad', Lastname: 'Haj', Age: 20, Height: 185, Wight: 88};
+let users = {Firstname: 'Mohammad', Lastname: 'Haj', Age: 20, Height: 185, Wight: 88}, 
+{Firstname: 'Mehdi', Lastname: 'Hej', Age: 21, Height: 190, Wight: 19};
 console.log(users.Firstname)
 
 console.log('Hej')
@@ -11,4 +12,4 @@ console.log(users)
 
 users.Firstname = 'Mohammad';
 
-console.log(`Hej mig navn er ${users.Firstname}${users.Lastname} `)
+console.log(`Hej mig navn er ${users.Firstname, users.Lastname} ${users.Lastname} `)
